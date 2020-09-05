2761f1ac12835e4594269d5ea0a61d4bce1b4728
YOUR NAME <YOUR@EMAIL.com>
2020-09-05 23:18:38 +0530

    inital commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  
+  root to: "channels#index"
+  resources :channels
+  devise_for :users
+  
+ # devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
